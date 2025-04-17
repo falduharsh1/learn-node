27529604527f94b764be42ab4488fcb67b969d17
falduharsh1 <falduharsh1@gmail.com>
2025-04-17 08:26:39 +0530

    jj

--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -1,16 +1,32 @@
+const Joi = require("joi");
 const pick = require("../helper/pick");
 
-const validate = (schema) => (req,res,next) => {
+const validate = (schema) => async (req, res, next) => {
     try {
-        console.log(schema,req,Object.keys(schema));
+        // console.log(schema, req, Object.keys(schema));
 
-        const obj = pick(req,Object.keys(schema))
+        const obj = pick(req, Object.keys(schema))
 
         console.log(obj);
+
+        const {value,error} = await 
+        Joi.compile(schema)
+        .prefs({
+            abortEarly : false
+        })
+        .validate(obj);
+
+        console.log(value,error);
         
-        
+        if(error){
+            const err = error.details.map((v) => v.message).join(",\n")
+
+            console.log(err);
+        }
+
+
     } catch (error) {
-        
+
     }
 }
 
